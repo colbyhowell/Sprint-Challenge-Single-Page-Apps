fd6e330bc3750e1f8477ecb6697206be467eb6cd
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-08-16 21:11:09 -0500

    part one is complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import TabExampleBasic from "./components/TabNav.js";
+import MenuExampleBasic from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
+import LocationList from "./components/LocationsList";
+import EpisodeList from "./components/EpisodesList";
 
 export default function App() {
   return (
     <div className="wrapper-div">
       <main>
         <Header />
-        <TabExampleBasic />
+        <MenuExampleBasic />
         <Route exact path="/" component={WelcomePage} />
-        <Route path="/characters/" component={CharacterList} />
+        <Route path="/characters" component={CharacterList} />
+        <Route path="/locations" component={LocationList} />
+        <Route path="/episodes" component={EpisodeList} />
       </main>
     </div>
   );
--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 
-export default function LocationList() {
+export default function EpisodeList() {
   // TODO: Add useState to track data from useEffect
 
-  const [locationList, setLocationList] = useState([]);
+  const [episodeList, setEpisodeList] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    Axios.get("https://rickandmortyapi.com/api/location/")
-      .then(locals => {
-        console.log(locals);
-        setLocationList(locals.data.results);
+    Axios.get("https://rickandmortyapi.com/api/episode/")
+      .then(eps => {
+        console.log(eps);
+        setEpisodeList(eps.data.results);
       })
       .catch(err => {
         console.log(err);
@@ -22,8 +22,8 @@ export default function LocationList() {
   return (
     <section className="location-list grid-view">
       <h2>
-        {locationList.map(locals => {
-          return `${locals.name}`;
+        {episodeList.map(eps => {
+          return `${eps.name}`;
         })}
       </h2>
     </section>
